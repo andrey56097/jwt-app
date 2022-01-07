daa77fef061ec312946b142cb72e765223cb4be5
andrey56097 <batsandrey@gmail.com>
2022-01-07 20:19:37 +0200

    base config

---
+++ src/main/java/com/batsandrey/jwt/service/UserService.java
@@ -0,0 +1,19 @@
+package com.batsandrey.jwt.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Todo create logic to get user from the Db
+        return new User("admin", "pass", new ArrayList<>());
+    }
+}
